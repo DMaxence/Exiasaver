9fc8623f946a20b8019a3bfd877ca2647d54b4ce
Paul <abonnementspaul@gmail.com>
2016-12-07 15:35:30 +0100

    structure

--- common/readPBM.c
+++ common/readPBM.c
@@ -12,13 +12,45 @@
 
 #include "readPBM.h"
 
-int readMetaData(char[] fileName, int * x, int * y)
-{
+#define SIZELINE 255
 
+int readMetaData(char[] fileName, int * x, int * y, int * RGB)
+{
+	FILE* file = fopen(fileName, "r");
+	char line[SIZELINE];
+	int stop = 0;
+	
+	//We check if the the file is successfully opened
+	if(file == NULL)
+	{
+		puts("Erreur, le fichier suivant n'a pas pu être ouvert:");
+		puts(fileName);
+		exit(1);
+	}
+
+	//We read each line. We have to make sure we read data in the right order
+
+	//First we have to find the magic number
+	while(stop == 1)
+	{
+		if(fgets(line, SIZELINE, file) == NULL)
+		{
+			puts("Erreur, le fichier suivant ne possede pas de 'magic number':");
+			puts(fileName);
+			exit(1);
+		}
+
+		//We just wait for a line that starts with P
+		if (line[0] == 'P')
+		{
+			*RGB = line[1] - '0'; //Converts char to int
+			stop  = 1;
+		}
+	}
 }
 
 
 int readPBM(char[] fileName, int * imageWidth, int * imageHeight, char ** output[])
 {
-	
+
 }
--- common/readPBM.h
+++ common/readPBM.h
@@ -12,14 +12,16 @@
 
 #pragma once
 #include <stdio.h>
+#include <stdlib.h>
 
 //readMetaData
 // OUTPUT - error code
 // OUTPUT x - image width
 // OUTPUT y - image height
+// OUTPUT RGB - Whether or not the image is RGB (0 -> black and white ; 1 -> RGB)
 // INPUT fileName - name of image to parse
 // NOTE - only accepts RGB images
-int readMetaData(char[] fileName, int * x, int * y);
+int readMetaData(char[] fileName, int * x, int * y, int * RGB);
 
 //readPBM
 // INPUT fileName - file name
