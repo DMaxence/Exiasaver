f7696d8184e836a249c90afb81f723208f148b08
DMaxence <maxence.duhoux@viacesi.fr>
2016-12-07 15:52:53 +0100

    ajout des fichiers

---
+++ launcher/launcher.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   launcher.h                                      __       __ __	          */
+/*                                                  |    \  /   |       /\    */
+/*   By: mduhoux <maxence.duhoux@viacesi.fr>        |__   \/    |      /  \   */
+/*                                                  |     /\    |     /____\  */
+/*   Created: 2016/12/07 15:45:13 by mduhoux        |__  /  \ __|__  /      \ */
+/*   Updated: 2016/12/07 15:52:28 by mduhoux                                  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+//fonction empty pour nettoyer le terminal
+void	empty();
