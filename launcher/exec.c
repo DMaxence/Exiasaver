6b9c4b7d1734d92c07f3b89d7e235ce16bf6e6d7
DMaxence <maxence.duhoux@viacesi.fr>
2016-12-09 17:35:43 +0100

    ajout de fichiers

--- launcher/exec.c
+++ launcher/exec.c
@@ -6,8 +6,31 @@
 /*   By: mduhoux <maxence.duhoux@viacesi.fr>        |__   \/    |      /  \   */
 /*                                                  |     /\    |     /____\  */
 /*   Created: 2016/12/09 09:54:30 by mduhoux        |__  /  \ __|__  /      \ */
-/*   Updated: 2016/12/09 09:58:34 by mduhoux                                  */
+/*   Updated: 2016/12/09 17:35:16 by mduhoux                                  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "launcher.h"
+
+int		main(void)
+{
+	pid_t pid_child;
+
+	pid_child = fork();
+
+	switch(pid_child)
+	{
+	case -1:
+		perror("fork");
+		return EXIT_FAILURE;
+	break;
+
+	case 0:
+		execl(randomSaver(), randomImage(), (char*) NULL);
+		break;
+
+	default:
+		wait(pid_child);
+		break;
+	}
+}
--- static/main.c
+++ static/main.c
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                                            */
-/*   randomSaver.c                                   __       __ __	      */
+/*   main.c                                          __       __ __	          */
 /*                                                  |    \  /   |       /\    */
 /*   By: mduhoux <maxence.duhoux@viacesi.fr>        |__   \/    |      /  \   */
 /*                                                  |     /\    |     /____\  */
-/*   Created: 2016/12/09 09:58:40 by mduhoux        |__  /  \ __|__  /      \ */
-/*   Updated: 2016/12/09 17:35:05 by mduhoux                                  */
+/*   Created: 2016/12/09 17:33:00 by mduhoux        |__  /  \ __|__  /      \ */
+/*   Updated: 2016/12/09 17:33:56 by mduhoux                                  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "launcher.h"
+#include "../launcher/launcher.h"
 
-int	randomSaver(void)
+int		main(int argc, char *argv[])
 {
-	int randomNumber;
-	char path;
-	srand(time(NULL));
-
-	randomNumber = rand()%3+1;
-
-	switch(randomNumber)
-	{
-		case 1:
-			path = "../static/main.c";
-		break;
-		case 2:
-			path = "../dynamic/main.c";
-		break;
-		case 3:
-			path = "../interactive/main.c";
-		break;
-	}
-	return path;
 
 }
