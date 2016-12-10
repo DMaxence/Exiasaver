90a53e053a732e299f9e7375715033a7aa0df7c2
Paul <abonnementspaul@gmail.com>
2016-12-10 17:29:44 +0100

    Updated

--- launcher/launcher.h
+++ launcher/launcher.h
@@ -6,7 +6,7 @@
 /*   By: mduhoux <maxence.duhoux@viacesi.fr>        |__   \/    |      /  \   */
 /*                                                  |     /\    |     /____\  */
 /*   Created: 2016/12/07 15:45:13 by mduhoux        |__  /  \ __|__  /      \ */
-/*   Updated: 2016/12/10 14:57:06 by mduhoux                                  */
+/*   Updated: 2016/12/10 16:25:33 by mduhoux                                  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,24 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+
 //fonction empty pour nettoyer le terminal
 void	empty(void);
 
-//focntion randomSaver pour definir un chiffre aleatoire dans le but d'executer un saver aleatoirement
+//fonction randomSaver pour definir un chiffre aleatoire dans le but d'executer un saver aleatoirement
 int		randomSaver(void);
 
 //fonction randomImage pour selectionner aleatoirement l'image du saver 1
-char*		randomImage(void);
+char*	randomImage(void);
 
 //fonction logs qui cree et modifie le fichier de logs du saver
 int		logs(char *ex);
 
 //fonction pour determiner la taille du terminal
 int		size_terminal(void);
+
+//fonction qui execute un saver aleatoirement grace a la fonction randomSaver
+int		executeRandom(void);
+
+//fonction pour recuperer les variables environnementales
+int		var_env(int saver);
