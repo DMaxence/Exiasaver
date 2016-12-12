ca769aac9bb4ac1fa72c4ea49cd63ed0906e2ece
root <root@debian>
2016-12-12 19:21:34 +0100

    add files

--- static/main.c
+++ static/main.c
@@ -9,10 +9,34 @@
 /*   Updated: 2016/12/09 17:33:56 by mduhoux                                  */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <string.h>
 #include "../launcher/launcher.h"
+#include "../common/struct_image.h"
+#include "../common/createUniformImage.h"
+#include "../common/readPBM.h"
+#include "../common/centerImage.h"
+#include "../common/printImage.h"
 
-int		main(int argc, char *argv[])
+    int		main(int argc, char *argv[])
 {
+      char imagename[255];
+
+      /*Vérification du paramètre de l'image*/
+      if (argc == 2)
+      {
+        strcpy (imagename, argv[1]);
+
+
+      }
+      image *background;
+      image *display;
+      display = malloc (sizeof(image));
+      background = createUniformImageTermSize(' ');
+      readPBM (imagename, display);
+      centerImage (*background, *display);
+      printImage (background);
+      return 0;
+
+
 
 }
