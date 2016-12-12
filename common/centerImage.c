1c7c4d9781903dee11ba0518775118e92e627056
DMaxence <maxence.duhoux@viacesi.fr>
2016-12-12 11:18:31 +0100

    add file centerImage.c

--- common/centerImage.c
+++ common/centerImage.c
@@ -1,6 +1,15 @@
 #include "centerImage.h"
 
-void centerImage(image backgound, image imageToCenter)
+void centerImage(image background, image imageToCenter)
 {
+	//determiner l'endroit a partir de laquelle il faut mettre l'image a centrer
+	int	x;
+	int	y;
 	
+	x = (background.xDim - imageToCenter.xDim) / 2;
+	y = (background.yDim - imageToCenter.yDim) / 2;
+
+	imageToCenter.xPos = x;
+	imageToCenter.yPos = y;
+	mergeImages(background, imageToCenter);
 }
