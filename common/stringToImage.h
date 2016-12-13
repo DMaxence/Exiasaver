5d57977e94ffa7ec029df693903ca47f8d70cee1
DMaxence <maxence.duhoux@viacesi.fr>
2016-12-13 10:31:17 +0100

    add file stringToImage

---
+++ common/stringToImage.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   stringToImage.h                                 __       __ __	          */
+/*                                                  |    \  /   |       /\    */
+/*   By: mduhoux <maxence.duhoux@viacesi.fr>        |__   \/    |      /  \   */
+/*                                                  |     /\    |     /____\  */
+/*   Created: 2016/12/13 10:08:55 by mduhoux        |__  /  \ __|__  /      \ */
+/*   Updated: 2016/12/13 10:29:58 by mduhoux                                  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "struct_image.h"
+#include "createUniformImage.h"
+#include <string.h>
+image *	stringToImage(char *string);
