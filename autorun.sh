3440429cb964c28756b8366184cf2a9c0168cf5e
DMaxence <maxence.duhoux@viacesi.fr>
2016-12-14 09:56:05 +0100

    add autorun.sh

---
+++ autorun.sh
@@ -0,0 +1,7 @@
+export EXIASAVER_HOME=$PWD/execfiles/
+export EXIASAVER1_PBM=$PWD/img/
+export EXIASAVER2_PBM=$PWD/img/numbers/
+export EXIASAVER2_TAILLE=3x5
+export EXIASAVER2_SLEEP=10
+export EXIASAVER3_PBM=$PWD/img/plane
+./$PWD/execfiles/launcher
