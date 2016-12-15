68c69c1623552aaf1b2b995803e513696434567b
DMaxence <maxence.duhoux@viacesi.fr>
2016-12-15 09:32:48 +0100

    add working pipe while fetching argv[2]

--- static/main.c
+++ static/main.c
@@ -6,7 +6,7 @@
 /*   By: mduhoux <maxence.duhoux@viacesi.fr>        |__   \/    |      /  \   */
 /*                                                  |     /\    |     /____\  */
 /*   Created: 2016/12/09 17:33:00 by mduhoux        |__  /  \ __|__  /      \ */
-/*   Updated: 2016/12/09 17:33:56 by mduhoux                                  */
+/*   Updated: 2016/12/15 09:26:56 by mduhoux                                  */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
@@ -20,38 +20,80 @@
 
 int		main(int argc, char *argv[])
 {
-      char imagename[255];
+    char imagename[255];
+	/*Déclaration de la variable de types image*/
+	image *background;
+	image *display;
+	
+	/*Creation des variables necessaires au fork et au pipe*/
+	int fd[2];
+	int readFromPipe;
+	pid_t pid_child;
+	char readBufferFromPipe[100];
+	char *stringToReturn;
 
-      /*Vérification du paramètre de l'image*/
-      if (argc == 2)
-      {
-            strcpy (imagename, argv[1]);
-      }
-       /*Déclaration de la variable de types image*/
-      image *background;
-      image *display;
+	/*Debut du fork et du pipe*/
 
-      /*Allocation de mémoire*/
-      display = malloc (sizeof(image));
+	if(pipe(fd) != 0)
+	{
+		perror ("pipe");
+		exit(1);
+	}
+	pid_child = fork();
 
-      /*Adaptation de l'image avec le terminal*/
-      background = createUniformImageTermSize(' ');
+	switch(pid_child)
+	{
+		case -1:
+			perror("fork");
+			exit(1);
+			break;
+		case 0:
+			/*Fermeture de fd[0] car l'enfant n'a pas besoin de lire dans le pipe*/
+			close(fd[0]);
 
-      /*Converti le fichier de façon à pouvoir afficher une image*/
-      readPBM (imagename, display);
+    		/*Vérification du paramètre de l'image*/
+		    if (argc == 2)
+		    {
+		          strcpy (imagename, argv[1]);
+		    }
+			
+			/*ecriture de imagename dans le pipe*/
+			write(fd[1], imagename, (sizeof(char) * strlen(imagename)+1));
+			close(fd[1]);
+			exit(0);
+			break;
+		default:
+			/*Fermeture de fd[0] car l'enfant n'a pas besoin de lire dans le pipe*/
+			close(fd[1]);
 
-      /*Permet de centrer l'image sur le terminal*/
-      centerImage (*background, *display);
+			/*On lit le nom du fichier recupere par le pipe*/
+			readFromPipe = read(fd[0], readBufferFromPipe, (sizeof(char) * 100));
+			
+			/*On copie le contenu de readBufferFromPipe dans imagename*/
+			strcpy(imagename, readBufferFromPipe);
 
-      /*Permet d'afficher l'image*/
-      printImage (background);
+			/*Allocation de mémoire*/
+		    display = malloc (sizeof(image));
 
-      /*Comme il n'y a qu'une frame à afficher, on peut tout supprimer*/
-      deleteImage(background);
-      deleteImage(display);
+		    /*Adaptation de l'image avec le terminal*/
+		    background = createUniformImageTermSize(' ');
 
-      /*Attend l'appuie d'une touche pour pouvoir quitter le programme*/
-      getchar();
+		    /*Converti le fichier de façon à pouvoir afficher une image*/
+		    readPBM (imagename, display);
 
-      return 0;
+		    /*Permet de centrer l'image sur le terminal*/
+		    centerImage (*background, *display);
+
+		    /*Permet d'afficher l'image*/
+		    printImage (background);
+
+		    /*Comme il n'y a qu'une frame à afficher, on peut tout supprimer*/
+		    deleteImage(background);
+		    deleteImage(display);
+
+		    /*Attend l'appuie d'une touche pour pouvoir quitter le programme*/
+		    getchar();
+
+    return 0;
+	}
 }
