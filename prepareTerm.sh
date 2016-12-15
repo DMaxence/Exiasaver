70233ef8e5d8fe52ced9a53e427334613a3eabf6
DMaxence <maxence.duhoux@viacesi.fr>
2016-12-15 10:33:04 +0100

    final commit with make.sh

---
+++ prepareTerm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export EXIASAVER_HOME=$PWD/execfiles/
+export EXIASAVER1_PBM=$PWD/img/
+export EXIASAVER2_PBM=$PWD/img/numbers/
+export EXIASAVER2_SLEEP=10
+export EXIASAVER2_TAILLE=3x5
+export EXIASAVER3_PBM=$PWD/img/plane/
+bash
